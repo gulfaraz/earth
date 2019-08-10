e48004f803340f3f30ee73470c60cb6806697628
Gulfaraz Yasin <gulfarazyasin@gmail.com>
2019-08-10 13:23:25 +0000

    share folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
-var gulp = require("gulp");
+var gulp = require('gulp');
 
-gulp.task("default", function () {
-    var express = require("express");
+gulp.task('default', function () {
+    var express = require('express');
+    var serveIndex = require('serve-index');
     var app = express();
     app.use(express.static(__dirname + '/node_modules'));
-    //, require('./routes/routes')(express));
+    app.use('/share', express.static(__dirname + '/share'));
+    app.use('/share', serveIndex(__dirname + '/share'));
     app.get('/font', function (req, res) {
         res.sendFile(__dirname + '/font.ttf');
+    });
+    app.get('/resume', function (req, res) {
+        res.sendFile(__dirname + '/share/gulfarazrahman.pdf');
     });
     app.get('/', function (req, res) {
         res.sendFile(__dirname + '/index.html');
